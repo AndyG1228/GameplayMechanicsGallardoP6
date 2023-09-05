65616fe1e6d3c3704d873fbd64c3d3cb7b8824d3
AndyG1228 <606925@student.dusd.net>
2023-09-05 13:48:44 -0700

    Completed main Unit
    Rocket Power is being developed.

--- GameplayMechanicsGallardoP6/Assets/Scripts/PlayerController.cs
+++ GameplayMechanicsGallardoP6/Assets/Scripts/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
 
+    public float powerupStrength;
     public float speed = 5.0f;
     public bool hasPowerup;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,6 +34,28 @@ public class PlayerController : MonoBehaviour
         {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + "with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
     }
 }
---
+++ GameplayMechanicsGallardoP6/Assets/Scripts/RocketBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBehaviour : MonoBehaviour
+{
+
+    private Transform target;
+    private float speed = 15.0f;
+    private bool homing;
+    private float rocketStrength = 15.0f;
+    private float aliveTimer =  5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
