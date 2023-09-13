0257221204e530d8b9f05bd56a122b31eedac4bc
AndyG1228 <606925@student.dusd.net>
2023-09-13 13:50:03 -0700

    KATAMARI
    Created katamari ball and uploaded recording, must add a particle to challenge scene to complete

--- GameplayMechanicsGallardoP6/Assets/Scripts/SpawnManager.cs
+++ GameplayMechanicsGallardoP6/Assets/Scripts/SpawnManager.cs
@@ -12,6 +12,10 @@ public class SpawnManager : MonoBehaviour
 
     private float spawnRange = 9;
 
+    public GameObject bossPrefab;
+    public GameObject[] miniEnemyPrefabs;
+    public int bossRound;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,6 +45,34 @@ public class SpawnManager : MonoBehaviour
         return randomPos;
     }
 
+    void SpawnBossWave (int currentRound)
+    {
+        int miniEnemysToSpawn;
+
+        if (bossRound != 0)
+        {
+            miniEnemysToSpawn = currentRound / bossRound;
+        }
+        else
+        {
+            miniEnemysToSpawn = 1;
+        }
+
+        var boss = Instantiate(bossPrefab, GenerateSpawnPosition(), bossPrefab.transform.rotation);
+        boss.GetComponent<Enemy>().miniEnemySpawnCount = miniEnemysToSpawn;
+    }
+
+    public void SpawnMiniEnemy(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            int randomMini = Random.Range(0,miniEnemyPrefabs.Length);
+            Instantiate(miniEnemyPrefabs[randomMini], GenerateSpawnPosition(), 
+                miniEnemyPrefabs[randomMini].transform.rotation);
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -48,7 +80,14 @@ public class SpawnManager : MonoBehaviour
         if (enemyCount == 0)
         {
             waveNumber++;
-            SpawnEnemyWave(waveNumber);
+            if (waveNumber % bossRound == 0)
+            {
+                SpawnBossWave(waveNumber);
+            }
+            else
+            {
+                SpawnEnemyWave(waveNumber);
+            }
             int randomPowerup = Random.Range(0, powerupPrefabs.Length); Instantiate(powerupPrefabs[randomPowerup], 
             GenerateSpawnPosition(), powerupPrefabs[randomPowerup].transform.rotation);
         }
